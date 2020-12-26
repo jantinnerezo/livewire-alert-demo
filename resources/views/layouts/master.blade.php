83b33e24cebbf71e6a82f3b368bf3c8aad1a4e04
Jantinn Erezo <erezojantinn@gmail.com>
2020-12-27 02:11:39 +0800

    Added SEO meta tags

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -10,6 +10,28 @@
             <title>{{ config('app.name') }}</title>
         @endif
 
+        <meta name="keywords" content="Laravel, Laravel Livewire, Livewire Alert, SweetAlert2, alert, Tailwind"/>
+        <meta name="description" content="This package provides a simple alert utilities for your livewire components.">
+        <meta name="subject" content="Livewire Alert">
+        <meta name="copyright" content="Jantinn Erezo">
+        <meta name="robots" content="index,follow" />
+
+        <!-- Open Graph -->
+        <meta name="og:title" content="{{ config('app.name') }}"/>
+        <meta name="og:type" content="website"/>
+        <meta name="og:url" content="https://livewire-alert.jantinnerezo.com/"/>
+        <meta name="og:image" content="https://repository-images.githubusercontent.com/272130835/4db85f80-4524-11eb-9a4c-b52cece2cf4b"/>
+        <meta name="og:site_name" content="{{ config('app.name') }}"/>
+        <meta name="og:description" content="This package provides a simple alert utilities for your livewire components."/>
+
+        <!-- Twitter -->
+        <meta name="twitter:card" content="https://repository-images.githubusercontent.com/272130835/4db85f80-4524-11eb-9a4c-b52cece2cf4b">
+        <meta name="twitter:site" content="{{ config('app.name') }}">
+        <meta name="twitter:creator" content="Jantinn Erezo">
+        <meta name="twitter:title" content="{{ config('app.name') }}">
+        <meta name="twitter:description" content="This package provides a simple alert utilities for your livewire components.">
+        <meta name="twitter:image:src" content="https://repository-images.githubusercontent.com/272130835/4db85f80-4524-11eb-9a4c-b52cece2cf4b">
+
         <link rel="shortcut icon" type="image/x-heroicon" href="{{ asset('static/logo/app-logo.png') }}">
 
         <!-- Fonts -->
