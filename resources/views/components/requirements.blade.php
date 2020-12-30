14087b3d2e73173e44966b217611b673c66a971a
Jantinn Erezo <erezojantinn@gmail.com>
2020-12-31 01:50:23 +0800

    :heavy_plus_sign: Added feature tests

--- resources/views/components/requirements.blade.php
+++ resources/views/components/requirements.blade.php
@@ -4,7 +4,7 @@
         This package uses <span> <a href="https://laravel-livewire.com"> Livewire </a> </span> under the hood. Please make sure you include  it in your dependencies before using this package.
     </p>
     <ul>
-        <li> <p class="text-cool-gray-600 my-0"> PHP >= 7.2 </p> </li>
+        <li> <p class="text-cool-gray-600 my-0"> PHP 7.2 or higher </p> </li>
         <li> <p class="text-cool-gray-600 my-0"> Laravel 7, 8 </p> </li>
         <li> <p class="text-cool-gray-600 my-0"> Livewire </p> </li>
         <li> <p class="text-cool-gray-600 my-0"> SweetAlert2 </p> </li>
